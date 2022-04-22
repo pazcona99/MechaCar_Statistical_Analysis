7f2312f61355a9f38180045d9384294779a1c952
Pablo Azcona <pablo.azcona@blach.com>
2022-04-22 14:13:34 -0700

    mpg model

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,9 @@ library(tidyverse)
 
 # Read CSV
 mechacar <-read_csv("Data/MechaCar_mpg.csv")
+
+#Linear Regression
+mecha_lm <- lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar)
+
+# Summary of linear regression model
+summary(mecha_lm)
